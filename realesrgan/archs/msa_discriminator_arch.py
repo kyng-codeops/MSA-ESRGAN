e23eb77740882b4038543c912ab18e52e917fa19
kyng-codeops <knveil.amazon@gmail.com>
2025-03-12 19:19:56 -0400

    fix: changed ReLU to LeakyReLU to solve vanishing gradiant during training

--- realesrgan/archs/msa_discriminator_arch.py
+++ realesrgan/archs/msa_discriminator_arch.py
@@ -9,7 +9,7 @@ class ChannelAttention(nn.Module):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.conv1 = nn.Conv2d(num_in_ch, num_in_ch // 2, 1, bias=False)
-        self.relu = nn.ReLU(inplace=True)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
         self.conv2 = nn.Conv2d(num_in_ch // 2, num_in_ch, 1, bias=False)
         self.batch_norm = nn.BatchNorm2d(num_in_ch)
 
@@ -19,11 +19,11 @@ class ChannelAttention(nn.Module):
 
     def forward(self, x):
         avg_out = self.conv1(self.avg_pool(x))
-        avg_out = self.relu(avg_out)
+        avg_out = self.lrelu(avg_out)
         avg_out = self.batch_norm(self.conv2(avg_out))
 
         conv_out = self.conv3(x)
-        conv_out = self.batch_norm2(self.relu(conv_out))
+        conv_out = self.batch_norm2(self.lrelu(conv_out))
 
         combined = avg_out + conv_out
         attention = self.sigmoid(combined)
@@ -57,12 +57,11 @@ class CSAFM(nn.Module):
 class DownBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DownBlock, self).__init__()
-        # self.conv = spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1))
         self.conv = spectral_norm(nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False))
-        self.relu = nn.ReLU(inplace=True)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
     def forward(self, x):
-        return self.relu(self.conv(x))
+        return self.lrelu(self.conv(x))
 
 class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -71,20 +70,18 @@ class UpBlock(nn.Module):
         self.conv = spectral_norm(
                 nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
             )
-        self.relu = nn.ReLU(inplace=True)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
     def forward(self, x):
-        # return self.relu(self.conv(self.upsample(x)))
-        return self.relu(self.conv(self.relu(self.upsample(x))))
+        return self.lrelu(self.conv(self.lrelu(self.upsample(x))))
 
 @ARCH_REGISTRY.register()
 class UNetDiscriminator(nn.Module):
     def __init__(self, num_in_ch=3, num_feat=64, skip_connection=False):
         super(UNetDiscriminator, self).__init__()
         self.skip_connection = skip_connection
-        self.relu = nn.ReLU(inplace=True)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
-        # self.encoder1 = DownBlock(num_in_ch, num_feat)
         self.encoder1 = nn.Conv2d(num_in_ch, num_feat, kernel_size=3, stride=1, padding=1)
         self.encoder2 = DownBlock(num_feat, num_feat * 2)
         self.encoder3 = DownBlock(num_feat * 2, num_feat * 4)
@@ -96,13 +93,9 @@ class UNetDiscriminator(nn.Module):
         self.csafm2 = CSAFM(num_feat * 2)
         self.up3 = UpBlock(num_feat * 2, num_feat)
         self.csafm3 = CSAFM(num_feat)
-        # self.up4 = UpBlock(num_feat, num_feat)
 
-        # self.final_conv1 = spectral_norm(nn.Conv2d(num_feat, num_feat, kernel_size=3, padding=1))
-        # self.final_conv2 = spectral_norm(nn.Conv2d(num_feat, 1, kernel_size=3, padding=1))
         self.final_conv1 = spectral_norm(nn.Conv2d(num_feat, num_feat, 3, 1, 1, bias=False))
         self.final_conv2 = spectral_norm(nn.Conv2d(num_feat, 1, 3, 1, 1))
-
 
     def forward(self, x):
         # NOTE: THERE ARE NO SKIPCONNECTIONS?
@@ -134,8 +127,8 @@ class UNetDiscriminator(nn.Module):
         d3 = self.up3(d2)
         d3 = self.csafm3(d3, e1)
 
-        out = self.relu(self.final_conv1(d3))
-        out = self.relu(self.final_conv1(out))
-        out = self.relu(self.final_conv2(out))
+        out = self.lrelu(self.final_conv1(d3))
+        out = self.lrelu(self.final_conv1(out))
+        out = self.lrelu(self.final_conv2(out))
 
         return out
